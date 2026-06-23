8c9f7dfeb55c5972330cedf4bbad116a56b79475
elias.lima@aluno.cps.sp.gov.br <elias.lima@aluno.cps.sp.gov.br>
2026-06-23 13:49:56 -0300

    feat: sistema finalizado

--- caixa_eletronico/caixa_eletronico.c
+++ caixa_eletronico/caixa_eletronico.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#inclue <stdio.h>
 
 int main() {
     int opcao;
@@ -7,7 +7,7 @@ int main() {
     float emprestimo = 0;
 
     do {
-        printf("\n===== CAIXA ELETRONICO =====\n");
+        printf("\n= CAIXA ELETRONICO =\n");
         printf("1 - Verificar saldo\n");
         printf("2 - Depositar\n");
         printf("3 - Sacar\n");
@@ -19,7 +19,7 @@ int main() {
         scanf("%d", &opcao);
 
         if (opcao == 0) {
-            printf("Saindo... Ate logo!\n");
+            printf("Saindo\n");
         }
    } while (opcao != 0);
 
@@ -28,7 +28,7 @@ int main() {
 
 if (opcao == 1) {
             printf("Seu saldo e: R$ %.2f\n", saldo);
-        }
+        }=== 
         if (opcao == 2) {
             printf("Quanto quer depositar? R$ ");
             scanf("%f", &valor);
@@ -52,4 +52,44 @@ if (opcao == 1) {
                 printf("Saque realizado! Saldo restante: R$ %.2f\n", saldo);
             }
         }
+
+        if (opcao == 4) {
+            if (emprestimo > 0) {
+                printf("Voce ja tem um emprestimo de R$ %.2f. Pague antes de pedir outro.\n", emprestimo);
+            } else {
+                printf("Qual valor do emprestimo? R$ ");
+                scanf("%f", &valor);
+                if (valor <= 0) {
+                    printf("Valor invalido!\n");
+                } else {
+                    emprestimo = valor;
+                    saldo = saldo + valor;
+                    printf("Emprestimo aprovado! Novo saldo: R$ %.2f\n", saldo);
+                }
+            }
+        }
+        if (opcao == 5) {
+            if (emprestimo == 0) {
+                printf("Voce nao tem emprestimo ativo.\n");
+            } else {
+                printf("Sua divida e: R$ %.2f\n", emprestimo);
+                printf("Quanto quer pagar? R$ ");
+                scanf("%f", &valor);
+                if (valor <= 0) {
+                    printf("Valor invalido!\n");
+                } else if (valor > saldo) {
+                    printf("Saldo insuficiente!\n");
+                } else {
+                    if (valor > emprestimo) valor = emprestimo;
+                    saldo -= valor;
+                    emprestimo -= valor;
+                    printf("Pagamento realizado! Divida restante: R$ %.2f\n", emprestimo);
+                }
+            }
+        }
+        if (opcao == 6) {
+            printf("Situacao do emprestimo: R$ %.2f\n", emprestimo);
+        }
+
+
 
